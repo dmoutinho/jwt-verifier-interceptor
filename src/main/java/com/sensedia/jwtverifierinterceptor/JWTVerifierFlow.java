01d92d10863f9c3aa48ac73737d56d6ca4e60ee4
Diogo Moutinho <dmoutinho@gmail.com>
2019-06-21 17:50:50 -0300

    Update JWTVerifierFlow.java

--- src/main/java/com/sensedia/jwtverifierinterceptor/JWTVerifierFlow.java
+++ src/main/java/com/sensedia/jwtverifierinterceptor/JWTVerifierFlow.java
@@ -1,7 +1,13 @@
 package com.sensedia.jwtverifierinterceptor;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
@@ -10,6 +16,7 @@ import java.util.Base64;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.impl.JWTParser;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.sensedia.interceptor.externaljar.annotation.ApiSuiteInterceptor;
 import com.sensedia.interceptor.externaljar.annotation.InterceptorMethod;
@@ -63,14 +70,80 @@ public class JWTVerifierFlow {
 	
 	}
 	
+	private static void getPkFromCertificate() throws Exception {
+		
+		try {
+
+			String strCertificate = "-----BEGIN CERTIFICATE-----\n" + 
+					"MIIFRDCCBCygAwIBAgISA86Z9KQOk7pYx7qCCUBPG/a6MA0GCSqGSIb3DQEBCwUA\n" + 
+					"MEoxCzAJBgNVBAYTAlVTMRYwFAYDVQQKEw1MZXQncyBFbmNyeXB0MSMwIQYDVQQD\n" + 
+					"ExpMZXQncyBFbmNyeXB0IEF1dGhvcml0eSBYMzAeFw0xOTA2MTMwMDQ0NTZaFw0x\n" + 
+					"OTA5MTEwMDQ0NTZaMBExDzANBgNVBAMTBmp3dC5pbzCCASIwDQYJKoZIhvcNAQEB\n" + 
+					"BQADggEPADCCAQoCggEBAMz/b0bD8C9n9wFOp41NpK5zx+xEvoNadQDoLyavuxC1\n" + 
+					"cxsmn77c3g1/jwcahgpc9xv3cvjirGFV/eVmve47B6lLa6+sUyWCt5GygtNbfsG+\n" + 
+					"UeCYHGMYLw50RBwyuoV5FToujLQqiiANkpq+0bW3xuMwg87bG+3vDPNJrPpfkYjp\n" + 
+					"Hq8b9kOG3WWkxCsK0qRMGmJaEEqiSGRaWSUcggChfLbi7jwndwbN0oW7WFOCeRkQ\n" + 
+					"LM0ElgLjhtiV3JWa5dFmBq0UNS8WFv/cICAJIYmTz+RYFwVVyxUg2sVN3AJmZ5Dc\n" + 
+					"Y4pnX1t5VEOXBuWGhSYNVC5fPvU5WgDBvQqPx+KllS8CAwEAAaOCAlswggJXMA4G\n" + 
+					"A1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwDAYD\n" + 
+					"VR0TAQH/BAIwADAdBgNVHQ4EFgQUWwFEKMYcUup+qO0RZUg0Me73lOowHwYDVR0j\n" + 
+					"BBgwFoAUqEpqYwR93brm0Tm3pkVl7/Oo7KEwbwYIKwYBBQUHAQEEYzBhMC4GCCsG\n" + 
+					"AQUFBzABhiJodHRwOi8vb2NzcC5pbnQteDMubGV0c2VuY3J5cHQub3JnMC8GCCsG\n" + 
+					"AQUFBzAChiNodHRwOi8vY2VydC5pbnQteDMubGV0c2VuY3J5cHQub3JnLzARBgNV\n" + 
+					"HREECjAIggZqd3QuaW8wTAYDVR0gBEUwQzAIBgZngQwBAgEwNwYLKwYBBAGC3xMB\n" + 
+					"AQEwKDAmBggrBgEFBQcCARYaaHR0cDovL2Nwcy5sZXRzZW5jcnlwdC5vcmcwggEE\n" + 
+					"BgorBgEEAdZ5AgQCBIH1BIHyAPAAdgDiaUuuJujpQAnohhu2O4PUPuf+dIj7pI8o\n" + 
+					"kwGd3fHb/gAAAWtOgmKYAAAEAwBHMEUCIQDDWyrQAjpDEcb8odMLQtb404F++WQs\n" + 
+					"Vj4c939Rgh/WaQIgW4kvd751LZ6jHk40hKkih83RFku8qUp/5pc7FUhdVpsAdgBj\n" + 
+					"8tvN6DvMLM8LcoQnV2szpI1hd4+9daY4scdoVEvYjQAAAWtOgmK8AAAEAwBHMEUC\n" + 
+					"IDR260Zxiq0N5u46Jk8H9b6luQb+ZlumEEjwFqeMaoLcAiEAuRfLJLKiv3EDkogY\n" + 
+					"d9phRLc/E14PYE/lMY7xzDHLmmswDQYJKoZIhvcNAQELBQADggEBAJTYHcBN2uT/\n" + 
+					"vJpOVTjj0Hy/NmdA56rAT1xA5KbKBBATY9f1KNlWdYlAhZLoI/Txj3uU6d0+RrT9\n" + 
+					"e+QqD14oHR62ZIhuHZZ96pcjLOPfqYBFr2qmgUR0yxmMQ5ugGAw1Q5IZkTbCUnIO\n" + 
+					"l1rX/vfKrLxXDnEABw9inyX63TUUIu3Db2H6SaA41+AVLvM/UT8B5fbUN7WSfW15\n" + 
+					"zxS7L7knFF0ZKhb3WJxUAQvWSRMKXtWdeCwle1nKb0PWZm1sfoSHAsZU2EuR3PM2\n" + 
+					"RoKZSB4C9H/zFvwVfMIP8QWDpcduyudP51ZPc/w/fQbH9FXdY76ahUq3y+1sx3Kj\n" + 
+					"hMwKs3vuJhM=\n" + 
+					"-----END CERTIFICATE-----\n" + 
+					"";
+			
+			InputStream stream = new ByteArrayInputStream(strCertificate.getBytes(StandardCharsets.UTF_8));
+
+			CertificateFactory f = CertificateFactory.getInstance("X.509");
+			
+			X509Certificate certificate = (X509Certificate)f.generateCertificate(stream);			
+			
+			PublicKey pk = certificate.getPublicKey();
+			
+			System.out.println("RSA PK: "+new String(Base64.getEncoder().encode(pk.getEncoded())));
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		
+		}
+	
+	}
+
+	private static void testJWTVerifier() throws Exception {
+		JWTVerifierFlow jwt = new JWTVerifierFlow();
+		jwt.jwtVerifier(jwt.getRSAPublicKey(pk),token);
+		System.out.println("OK");
+	}
+	
 	//java -cp jwt-verifier-interceptor-1.0.0-jar-with-dependencies.jar com.sensedia.jwtverifierinterceptor.JWTVerifierFlow 
 	public static void main(String[] args) {
+
 		try {
-			JWTVerifierFlow jwt = new JWTVerifierFlow();
-			jwt.jwtVerifier(jwt.getRSAPublicKey(pk),token);
+			
+			getPkFromCertificate();
+			
+			//testJWTVerifier();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 		
 }
